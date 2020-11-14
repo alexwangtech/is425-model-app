710c1a3e0e8834af837bbbbb5d2997e6d583c36c
ray-springfield <48783729+ray-springfield@users.noreply.github.com>
2020-11-14 07:45:27 -0500

    Update index.js
    attempting to listen to all ports

--- index.js
+++ index.js
@@ -34,6 +34,6 @@ app.get('/test', (req, res) => {
     res.send("Test!");
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
